57d989a7370f71d7b66fd1deccb4801c62bbd2f6
fandog <84146392+fandogg@users.noreply.github.com>
2024-02-14 20:44:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function playSound() {
   var audio = new Audio('https://fandogg.github.io/fandogg/Fnaf%20Nose%20honk.mp3');
   audio.play();
+  location.replace("https://fandogg.github.io/fandogg/mafate/")
 }
